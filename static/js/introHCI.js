eb9cdc6977a9d30269a154df45f9d03bcd4e41bf
Xinyu Shao <x2shao@ucsd.edu>
2018-01-24 17:01:13 -0800

    commit messgafe

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -10,9 +10,26 @@ $(document).ready(function() {
  */
 function initializePage() {
 	$("#testjs").click(function(e) {
-		$('.jumbotron h1').text("Javascript is connected");
-	});
+		 $(".jumbotron h1") .text("Javascript has taken control");
+		 $("#jumbotron p").toggleClass("active");
+		 $("#testjs").text("Please wait...");
 
+
+		 
+	});
+    $("a.thumbnail").click(projectClick);
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
+
+}
+function projectClick(e) { 
+    // prevent the page from reloading      
+    e.preventDefault();
+    // In an event handler, $(this) refers to      
+    // the object that triggered the event      
+   
+   
+   
+    var containingProject = $(this).closest(".project");
+    containingProject.append("<div class='project-description'><p>Description of the project.</p></div>");
 }
